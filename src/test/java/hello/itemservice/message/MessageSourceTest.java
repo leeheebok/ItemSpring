a4e5a6979d9d0ecb13a38470804825392af3ba80
heebok <lehebk@naver.com>
2021-07-12 15:30:21 +0900

    Web Application Messages

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -1,12 +1,10 @@
 package hello.itemservice.message;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Locale;
 
